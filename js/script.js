32e32c6f53c8407b5202b3df625d99dafdeb6ed0
junsuwhy <junsuwhy@gmail.com>
2017-01-30 22:15:32 +0800

    Change style to roll text.

--- js/script.js
+++ js/script.js
@@ -4,22 +4,36 @@
     $w.resize(resizeWindow);
     resizeWindow();
     
+    interval_time = 1500;
+    
     var re = /[\?&]text=([^\&]+)/;
     var text = location.search;
     var search = text.match(re);
     if(search){
-      text = search[1];
-      $('.text').text(decodeURI(text));
+      text = decodeURI(search[1]);
+      $('.text').text(text);
+    }else{
+      text = '祝您2017年雞會滾滾來';
+    }
+    
+    kill_time = (text.length + 3)*interval_time;
+    
+    for(var i = 0; i <= text.length; i++){
+      (function(i){
+        setTimeout(function(){
+          addItem('.item-template.word',text[i]);
+        },interval_time * (i + 3) );
+      })(i);
     }
     
     
     addItem('.chi1');
     setTimeout(function(){
       addItem('.chi2');
-    },3333);
+    },interval_time * 1);
     setTimeout(function(){
       addItem('.chi3');
-    },6666);
+    },interval_time * 2);
     
   });
   
@@ -33,15 +47,20 @@
     $('.stat').append($('<div>').text(msg));
   }
   
-  function addItem(name){
+  function addItem(name, word){
     var $item = $(name).clone();
+    if(word){
+      $item.find('div').text(word);
+    }
     $item.appendTo('.canva').removeClass('item-template').addClass('item animation-action');
     setTimeout(function(){
       $item.find('.animation-peck').removeClass('animation-peck');
     },2500);
     setTimeout(function(){
+      addItem(name,word);
+    },kill_time);
+    setTimeout(function(){
       removeItem($item);
-      addItem(name);
     },10000);
     $item.css('left','100%');
     return $item;
